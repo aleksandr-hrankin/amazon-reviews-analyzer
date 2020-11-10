257cf92b19226c8914fe9de18963b1a8dd8fd374
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-11-10 15:22:08 +0200

    Init

---
+++ src/main/java/ua/antibyte/analyzer/AmazonReviewsAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ua.antibyte.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonReviewsAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonReviewsAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/antibyte/analyzer/AmazonReviewsAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.antibyte.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewsAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
