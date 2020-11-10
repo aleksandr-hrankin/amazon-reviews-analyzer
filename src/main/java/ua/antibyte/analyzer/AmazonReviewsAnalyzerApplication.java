94a6ef00acd8317d4ab4acc884d958965e7d748c
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-11-10 20:51:54 +0200

    Added CsvReader

--- src/main/java/ua/antibyte/analyzer/AmazonReviewsAnalyzerApplication.java
+++ src/main/java/ua/antibyte/analyzer/AmazonReviewsAnalyzerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AmazonReviewsAnalyzerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AmazonReviewsAnalyzerApplication.class, args);
     }
-
 }
