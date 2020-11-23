96d18f8841b4ab2365acbbd0292c60cb23cf17f6
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-11-23 20:28:47 +0200

    Added swagger

--- src/main/java/ua/antibyte/analyzer/controller/ProductController.java
+++ src/main/java/ua/antibyte/analyzer/controller/ProductController.java
@@ -1,5 +1,7 @@
 package ua.antibyte.analyzer.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,10 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/most-commented")
+    @GetMapping(value = "/most-commented")
+    @ApiOperation("Get a list of the most commented products")
     public List<ProductResponceDto> getMostCommented(
+            @ApiParam(value = "You can enter the number of products to return")
             @RequestParam(defaultValue = PAGE_SIZE) int quantity) {
         return productService.findMostCommented(quantity);
     }
